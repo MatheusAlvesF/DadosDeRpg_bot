463d0c47d6b3c5846a6de519eb7b0884a3f4d3dd
MatheusAlvesF <kaminasama777@gmail.com>
2025-04-11 12:46:20 -0300

    Adicionando função de critico

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ import re
 #@DadosDeRpg_bot
 #RpgDiceBot
 
-bot = telebot.TeleBot('7915216576:AAFnQyHByVkvAv1mXaWkuFhO9-N5eyMiMSc')
+bot = telebot.TeleBot('xxx')
 
 @bot.message_handler(commands=['start', 'help'])
 def start(msg: telebot.types.Message):
-    bot.reply_to(msg, '🎲 Olá! Use o comando /rolar no formato: 1d6, 2d6+1, 3d6-2, etc.')
+    bot.reply_to(msg, '🎲 Olá! Use o comando /r no formato: 1d6, 2d6+1, 3d6-2, etc.')
+
+
+# Handler para o comando /rolar
+@bot.message_handler(commands=['r'])
+def rolar(msg: telebot.types.Message):
+    try:
+        texto = msg.text.split(' ', 1)
+        if len(texto) < 2:
+            bot.reply_to(msg, "📌 Use: /r 1d6 ou 2d6+1, etc.")
+            return
+        comando = texto[1]
+        resultado = rolar_dados(comando)
+        bot.reply_to(msg, resultado)
+    except Exception as e:
+        bot.reply_to(msg, f"❌ Erro: {str(e)}")
 
 # Função para interpretar e rolar dados
 def rolar_dados(comando: str) -> str:
@@ -27,21 +42,12 @@ def rolar_dados(comando: str) -> str:
         return "⚠️ Calma aí! Tenta um número menor de dados ou lados."
 
     resultados = [random.randint(1, lados) for _ in range(qtd)]
-    total = sum(resultados) + modificador
-    return f"🎲 Rolando {comando}:\n🎯 Dados: {resultados}\n📊 Total: {total}"
 
-# Handler para o comando /rolar
-@bot.message_handler(commands=['r'])
-def rolar(msg: telebot.types.Message):
-    try:
-        texto = msg.text.split(' ', 1)
-        if len(texto) < 2:
-            bot.reply_to(msg, "📌 Use: /r 1d6 ou 2d6+1, etc.")
-            return
-        comando = texto[1]
-        resultado = rolar_dados(comando)
-        bot.reply_to(msg, resultado)
-    except Exception as e:
-        bot.reply_to(msg, f"❌ Erro: {str(e)}")
+    if resultados[0] == lados and modificador != 0:
+        total = sum(resultados) + (modificador * 2)
+        return f"🎲 Rolando {comando}:\n🎯 Dados: {resultados}\n💀 Crítico: {total}"
+    else:
+        total = sum(resultados) + modificador
+        return f"🎲 Rolando {comando}:\n🎯 Dados: {resultados}\n📊 Total: {total}"
 
 bot.infinity_polling()
